7bb86af00690a1cee733614485de181ebd5229cb
Paul Chheang <phalcheat@gmail.com>
2020-02-17 10:29:33 -0600

    Removed unnecessary codes

--- server.js
+++ server.js
@@ -4,12 +4,9 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const TestProdList = require("./models/product-Data");
 
-// const passport = require("passport");
-
 const productsRT = require("./routes/api/product-routes");
 const customersRT = require("./routes/api/users-routes");
 const ordersRT = require("./routes/api/order-routes");
-// const games = require("./routes/api/games");
 
 const port = process.env.PORT || 5000;
 
@@ -50,24 +47,11 @@ mongoose
   })
   .catch(err => console.log(err));
 
-
-
-
-
 // Routes
-app.get("/api/productsTest", (req, res) => res.json(TestProdList));
-app.get("/api/productsTest/:id", (req, res) => {
-  for (const p of TestProdList) {
-    if (p.id === req.params.id) {
-      return res.json(p);
-    }
-  }
-  res.json(TestProdList);
-});
 app.get("/", (req, res) => res.send("server is running"));
 app.use("/api/products", productsRT);
 app.use("/api/users", customersRT);
 app.use("/api/orders", ordersRT);
 
-
+// Start server
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
